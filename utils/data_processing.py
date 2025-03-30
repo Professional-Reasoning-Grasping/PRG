726df7a26d08c95781fe29fc46b7255c2f2fa873
yixing-ccc <81785077+yixing-ccc@users.noreply.github.com>
2025-03-31 01:20:04 +0800

    Add files via upload

---
+++ utils/data_processing.py
@@ -0,0 +1,57 @@
+import glob
+import json
+import os
+
+import cv2
+import numpy as np
+
+
+def get_sent_from_json(json_path, img):
+    try:
+        with open(json_path, "r") as r:
+            anno = json.loads(r.read())
+    except:
+        with open(json_path, "r", encoding="cp1252") as r:
+            anno = json.loads(r.read())
+
+    inform = anno["shapes"]
+    comments = anno["text"]
+    is_sentence = anno["is_sentence"]
+
+    height, width = img.shape[:2]
+
+    ### sort polies by area
+    area_list = []
+    valid_poly_list = []
+    for i in inform:
+        label_id = i["label"]
+        points = i["points"]
+        if "flag" == label_id.lower():  ## meaningless deprecated annotations
+            continue
+
+        tmp = np.zeros((height, width), dtype=np.uint8)
+        cv2.polylines(tmp, np.array([points], dtype=np.int32), True, 1, 1)
+        cv2.fillPoly(tmp, np.array([points], dtype=np.int32), 1)
+        tmp_area = tmp.sum()
+
+        area_list.append(tmp_area)
+        valid_poly_list.append(i)
+
+    sort_index = np.argsort(area_list)[::-1].astype(np.int32)
+    sort_index = list(sort_index)
+    sort_inform = []
+    for s_idx in sort_index:
+        sort_inform.append(valid_poly_list[s_idx])
+
+    for i in sort_inform:
+        label_id = i["label"]
+        points = i["points"]
+
+        if "ignore" in label_id.lower():
+            label_value = 255  # ignored during evaluation
+        else:
+            label_value = 1  # target
+
+    return comments, is_sentence
+
+
---
+++ utils/dataset.py
@@ -0,0 +1,365 @@
+import glob
+import os
+import random
+
+import cv2
+import numpy as np
+import torch
+import torch.nn.functional as F
+from transformers import CLIPImageProcessor
+
+from model.llava import conversation as conversation_lib
+from model.llava.constants import (DEFAULT_IMAGE_TOKEN, IGNORE_INDEX,
+                                   IMAGE_TOKEN_INDEX)
+from model.llava.mm_utils import tokenizer_image_token
+from model.segment_anything.utils.transforms import ResizeLongestSide
+
+from .conversation import get_default_conv_template
+from .data_processing import get_sent_from_json
+from utils.data.cornell_data import CornellDataset
+from .prg_dataset import PRGDataset
+from .utils import (DEFAULT_IM_END_TOKEN, DEFAULT_IM_START_TOKEN,
+                    DEFAULT_IMAGE_TOKEN)
+from .vqa_dataset import VQADataset
+
+
+def collate_fn(
+    batch, tokenizer=None, conv_type="llava_v1", use_mm_start_end=True, local_rank=-1
+):
+    image_path_list = []
+    images_list = []
+    images_clip_list = []
+    conversation_list = []
+    masks_list = []
+    label_list = []
+    resize_list = []
+    questions_list = []
+    sampled_classes_list = []
+    offset_list = [0]
+    cnt = 0
+    inferences = []
+    for (
+        image_path,
+        images,
+        images_clip,
+        conversations,
+        masks,
+        label,
+        resize,
+        questions,
+        sampled_classes,
+        inference,
+    ) in batch:
+        image_path_list.append(image_path)
+        images_list.append(images)
+        images_clip_list.append(images_clip)
+        conversation_list.extend(conversations)
+        label_list.append(label)
+        masks_list.append(masks.float())
+        resize_list.append(resize)
+        questions_list.append(questions)
+        sampled_classes_list.append(sampled_classes)
+        cnt += len(conversations)
+        offset_list.append(cnt)
+        inferences.append(inference)
+
+    if use_mm_start_end:
+        # replace <image> token
+        for i in range(len(conversation_list)):
+            replace_token = DEFAULT_IMAGE_TOKEN
+            replace_token = (
+                DEFAULT_IM_START_TOKEN + replace_token + DEFAULT_IM_END_TOKEN
+            )
+            conversation_list[i] = conversation_list[i].replace(
+                DEFAULT_IMAGE_TOKEN, replace_token
+            )
+    input_ids = [
+        tokenizer_image_token(prompt, tokenizer, return_tensors="pt")
+        for prompt in conversation_list
+    ]
+    input_ids = torch.nn.utils.rnn.pad_sequence(
+        input_ids, batch_first=True, padding_value=tokenizer.pad_token_id
+    )
+    attention_masks = input_ids.ne(tokenizer.pad_token_id)
+
+    conv = conversation_lib.default_conversation.copy()
+    targets = input_ids.clone()
+
+    if conv_type == "llava_v1":
+        sep = conv.sep + conv.roles[1] + ": "
+    else:
+        sep = "[/INST] "
+    for conversation, target in zip(conversation_list, targets):
+        total_len = int(target.ne(tokenizer.pad_token_id).sum())
+
+        rounds = conversation.split(conv.sep2)
+        cur_len = 1
+        target[:cur_len] = IGNORE_INDEX
+        for i, rou in enumerate(rounds):
+            if rou == "":
+                break
+
+            parts = rou.split(sep)
+            # if len(parts) != 2:
+            #     break
+            assert len(parts) == 2, (len(parts), rou)
+            parts[0] += sep
+
+            if DEFAULT_IMAGE_TOKEN in conversation:
+                round_len = len(tokenizer_image_token(rou, tokenizer))
+                instruction_len = len(tokenizer_image_token(parts[0], tokenizer)) - 2
+            else:
+                round_len = len(tokenizer(rou).input_ids)
+                instruction_len = len(tokenizer(parts[0]).input_ids) - 2
+
+            target[cur_len : cur_len + instruction_len] = IGNORE_INDEX
+
+            cur_len += round_len
+        target[cur_len:] = IGNORE_INDEX
+
+        if False:
+            z = target.clone()
+            z = torch.where(z == IGNORE_INDEX, tokenizer.unk_token_id, z)
+            if local_rank == 0:
+                print(
+                    "conversation: ",
+                    conversation,
+                    "tokenizer.decode(z): ",
+                    tokenizer.decode(z),
+                )
+
+        if cur_len < tokenizer.model_max_length:
+            assert cur_len == total_len
+
+    if inferences[0] == False:
+        truncate_len = tokenizer.model_max_length - 255
+
+        if input_ids.shape[1] > truncate_len:
+            input_ids = input_ids[:, :truncate_len]
+            targets = targets[:, :truncate_len]
+            attention_masks = attention_masks[:, :truncate_len]
+
+    return {
+        "image_paths": image_path_list,
+        "images": torch.stack(images_list, dim=0),
+        "images_clip": torch.stack(images_clip_list, dim=0),
+        "input_ids": input_ids,
+        "labels": targets,
+        "attention_masks": attention_masks,
+        "masks_list": masks_list,
+        "label_list": label_list,
+        "resize_list": resize_list,
+        "offset": torch.LongTensor(offset_list),
+        "questions_list": questions_list,
+        "sampled_classes_list": sampled_classes_list,
+        "inference": inferences[0],
+        "conversation_list": conversation_list,
+    }
+
+
+class HybridDataset(torch.utils.data.Dataset):
+    pixel_mean = torch.Tensor([123.675, 116.28, 103.53]).view(-1, 1, 1)
+    pixel_std = torch.Tensor([58.395, 57.12, 57.375]).view(-1, 1, 1)
+    img_size = 1024
+    ignore_label = 255
+
+    def __init__(
+        self,
+        base_image_dir,
+        tokenizer,
+        vision_tower,
+        samples_per_epoch=500 * 8 * 2 * 10,
+        precision: str = "fp32",
+        image_size: int = 224,
+        num_classes_per_sample: int = 3,
+        exclude_val=False,
+        dataset="reason_grasp",
+        sample_rate=1,
+        vqa_data="llava_instruct_150k",
+        reason_grasp_data="ReasonGrasp|train",
+        explanatory=0.1,
+    ):
+        self.exclude_val = exclude_val
+        self.dataset = dataset
+        self.samples_per_epoch = samples_per_epoch
+        self.explanatory = explanatory
+        self.num_classes_per_sample = num_classes_per_sample
+        sample_rate = np.array(sample_rate)
+        self.sample_rate = sample_rate / sample_rate.sum()
+
+        self.base_image_dir = base_image_dir
+        self.image_size = image_size
+        self.tokenizer = tokenizer
+        self.precision = precision
+
+        self.datasets = dataset.split("||")
+
+        self.all_datasets = []
+        for dataset in self.datasets:
+            if dataset == "vqa":
+                self.all_datasets.append(
+                    VQADataset(
+                        base_image_dir,
+                        tokenizer,
+                        vision_tower,
+                        samples_per_epoch,
+                        precision,
+                        image_size,
+                        num_classes_per_sample,
+                        exclude_val,
+                        vqa_data,
+                    )
+                )
+            elif dataset == "reason_grasp":
+                self.all_datasets.append(
+                    PRGDataset(
+                        base_image_dir,
+                        tokenizer,
+                        vision_tower,
+                        samples_per_epoch,
+                        precision,
+                        image_size,
+                        num_classes_per_sample,
+                        exclude_val,
+                        reason_grasp_data,
+                        explanatory,
+                    )
+                )
+
+    def __len__(self):
+        return self.samples_per_epoch
+
+    def __getitem__(self, idx):
+        ind = np.random.choice(list(range(len(self.datasets))), p=self.sample_rate)
+        data = self.all_datasets[ind]
+        inference = False
+        return *data[0], inference
+
+
+class ValDataset(torch.utils.data.Dataset):
+    pixel_mean = torch.Tensor([123.675, 116.28, 103.53]).view(-1, 1, 1)
+    pixel_std = torch.Tensor([58.395, 57.12, 57.375]).view(-1, 1, 1)
+    img_size = 1024
+    ignore_label = 255
+
+    def __init__(
+        self,
+        base_image_dir,
+        tokenizer,
+        vision_tower,
+        val_dataset,
+        image_size=1024,
+    ):
+        self.base_image_dir = base_image_dir
+        splits = val_dataset.split("|")
+        if len(splits) == 2:
+            ds, split = splits
+            images = glob.glob(
+                os.path.join(self.base_image_dir, "reason_grasp", ds, split, "*.jpg")
+            )
+            self.images = images
+            self.data_type = "reason_grasp"
+
+        self.ds = ds
+        self.image_size = image_size
+        self.tokenizer = tokenizer
+        self.transform = ResizeLongestSide(image_size)
+        self.clip_image_processor = CLIPImageProcessor.from_pretrained(vision_tower)
+
+        self.cornell_dataset =  CornellDataset(file_path=base_image_dir,
+                             output_size=image_size,
+                      ds_rotate=0.0,
+                      random_rotate=True,
+                      random_zoom=True,
+                      include_depth=1,
+                      include_rgb=1)
+    def __len__(self):
+            return len(self.images)
+
+    def preprocess(self, x: torch.Tensor) -> torch.Tensor:
+        """Normalize pixel values and pad to a square input."""
+        # Normalize colors
+        x = (x - self.pixel_mean) / self.pixel_std
+
+        # Pad
+        h, w = x.shape[-2:]
+        padh = self.img_size - h
+        padw = self.img_size - w
+        x = F.pad(x, (0, padw, 0, padh))
+        return x
+
+    def __getitem__(self, idx):
+       
+
+        image_path = self.images[idx]
+        image = cv2.imread(image_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        json_path = image_path.replace(".jpg", ".json")
+        sampled_sents, is_sentence = get_sent_from_json(json_path, image)
+        sampled_sents = [sampled_sents[0]]
+
+        conversations = []
+        conv = conversation_lib.default_conversation.copy()
+        i = 0
+        while i < len(sampled_sents):
+            conv.messages = []
+            text = sampled_sents[i].strip()
+            if is_sentence:
+                conv.append_message(
+                    conv.roles[0],
+                    DEFAULT_IMAGE_TOKEN
+                    + "\n {} Please output grasp pose.".format(text),
+                )
+                conv.append_message(conv.roles[1], "[POSE].")
+            else:
+                conv.append_message(
+                    conv.roles[0],
+                    DEFAULT_IMAGE_TOKEN
+                    + "\n What is {} in this image? Please output grasp pose.".format(
+                        text
+                    ),
+                )
+                conv.append_message(conv.roles[1], "[POSE].")
+            conversations.append(conv.get_prompt())
+            i += 1
+
+        # preprocess image for clip
+        image_clip = self.clip_image_processor.preprocess(image, return_tensors="pt")[
+            "pixel_values"
+        ][0]
+
+        # preprocess image for sam
+        image = self.transform.apply_image(image)
+        resize = image.shape[:2]
+        image = self.preprocess(torch.from_numpy(image).permute(2, 0, 1).contiguous())
+
+        gtbbs = self.cornell_dataset.get_gtbb(idx)
+        pos_img, ang_img, width_img = gtbbs.draw((self.output_size, self.output_size))
+        pos = self.numpy_to_torch(pos_img)
+        cos = self.numpy_to_torch(np.cos(2 * ang_img)) 
+        sin = self.numpy_to_torch(np.sin(2 * ang_img)) 
+        width = self.numpy_to_torch(width_img)
+
+        grasp = [pos,cos,sin,width]
+
+        pos_stack = torch.from_numpy(np.stack([g[0] for g in grasp], axis=0))
+        cos_stack = torch.from_numpy(np.stack([g[1] for g in grasp], axis=0))
+        sin_stack = torch.from_numpy(np.stack([g[2] for g in grasp], axis=0))
+        width_stack = torch.from_numpy(np.stack([g[3] for g in grasp], axis=0))
+
+        grasp = [pos_stack, cos_stack, sin_stack, width_stack]
+        labels = torch.ones(pos_stack.shape[1], pos_stack.shape[2]) * self.ignore_label
+        inference = True
+
+        return (
+            image_path,
+            image,
+            image_clip,
+            conversations,
+            grasp,
+            labels,
+            resize,
+            None,
+            None,
+            inference,
+        )
